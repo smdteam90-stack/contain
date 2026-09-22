f24d3cc2c541b268612fbe53f8b804a7f5353b85
smdteam90-stack <smd.team90@gmail.com>
2026-09-22 16:01:39 +0330

    Update compileSdk version to 37 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.sam.contentlibrary.content_library"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
